4cd8a3efdbbae423fb1fbc6a72ac96cfd8142373
Hesam <hesam.hb.bagherpour@gmail.com>
2023-09-28 16:26:04 +0330

    [add] Emaj Editor Play Shortcut

---
+++ Assets/LegendsOfLearningSDK/Scripts/Editor/EmajEditorPlayShortcut.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+namespace LoL.Editor
+{
+    public class EmajEditorPlayShortcut
+    {
+        [MenuItem("Emaj/PlayFrom Init  #&%p")]
+        public static void PlayFromSplashScreen()
+        {
+            EditorSceneManager.OpenScene(EditorBuildSettings.scenes.First(a => a.path.Contains("init")).path);
+            EditorApplication.EnterPlaymode();   
+        }
+    }
+}
